93c389c2ff3c5d8c1a4dbffeb07de70b3269e071
Roberto Broseta Vigara <roberto@dekalabs.com>
2021-05-15 18:18:26 +0200

    Initial SPM support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "CHIPageControl",
+    products: [
+        .library(
+            name: "CHIPageControl",
+            targets: ["CHIPageControl"]),
+    ],
+    targets: [
+        .target(
+            name: "CHIPageControl",
+            dependencies: []),
+    ]
+)
---
+++ Tests/CHIPageControlTests/CHIPageControlTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import CHIPageControl
+
+    final class CHIPageControlTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(CHIPageControl().text, "Hello, World!")
+        }
+    }
